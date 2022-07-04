1b4e50644d0811ee66c3a20d7827f4d559fe32cc
bobozaka <86849544+bobozaka@users.noreply.github.com>
2022-07-04 13:34:34 +0300

    1
    1

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -107,7 +107,7 @@ export const AddPost = () => {
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
+      {/* <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
         Загрузить превью
       </Button>
       <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
@@ -124,7 +124,7 @@ export const AddPost = () => {
         </>
       )}
       <br />
-      <br />
+      <br /> */}
       <TextField
         classes={{ root: styles.title }}
         variant="standard"
